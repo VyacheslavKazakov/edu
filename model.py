9737dd4368aafcc5b4985a9a740b5dd798714a7b
VyacheslavKazakov <vyachka@gmail.com>
2019-01-25 23:35:34 +0500

    unittest mock 1.0

---
+++ model.py
@@ -0,0 +1,22 @@
+
+
+class MyClass:
+    def _get_connection(self):
+            print('get connection')
+
+    def func2(self):
+        print(4)
+        self._get_connection()
+        print(5)
+
+    def consumer(self):
+        test = 1
+        print(0)
+        channel = self._get_connection().channel()
+        print(1)
+        print(channel.basic(test))
+        print(2)
+        print(channel.clever())
+        print(3)
+
+        return
